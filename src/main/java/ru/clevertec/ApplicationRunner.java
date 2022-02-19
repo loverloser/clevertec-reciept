40d1287987d8466a93125be4b8c25f0cb1cd0d78
loverloser <timofeilobanovich@gmail.com>
2022-02-19 08:34:23 +0300

    remove pdf writer

--- src/main/java/ru/clevertec/ApplicationRunner.java
+++ src/main/java/ru/clevertec/ApplicationRunner.java
@@ -5,7 +5,6 @@ import ru.clevertec.entity.DiscountCard;
 import ru.clevertec.entity.Product;
 import ru.clevertec.factories.DiscountCardFactory;
 import ru.clevertec.factories.ProductFactory;
-import ru.clevertec.print.impl.ReceiptPrinter;
 
 import java.util.Map;
 
@@ -14,11 +13,6 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         DiscountCard discountCard = DiscountCardFactory.getInstance(args);
         Map<Product, Integer> products = ProductFactory.getInstance(args);
-        ReceiptPrinter receiptPrinter = new ReceiptPrinter();
-        receiptPrinter.printInPDF(products, discountCard);
-//        ReceiptFactory.writeHeader();
-//        ReceiptFactory.writeBody(products);
-//        ReceiptFactory.writeFooter(products, discountCard);
     }
 
 }
