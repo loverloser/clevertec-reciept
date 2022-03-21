576ac45330898a981d495f512434ec7c4faca4a3
loverloser <timofeilobanovich@gmail.com>
2022-03-21 16:17:59 +0300

    refactor constant names

--- src/main/java/ru/clevertec/validator/ProductValidator.java
+++ src/main/java/ru/clevertec/validator/ProductValidator.java
@@ -8,13 +8,13 @@ import static ru.clevertec.constants.ApplicationConstants.PRODUCT_PRODUCER_ID;
 
 public class ProductValidator {
 
-    private static final String idRegex = "^[0-9]\\d*";
-    private static final String nameRegex = "^[A-Za-zА-ЯА-я]\\w+";
-    private static final String priceRegex = "\\d+\\.?\\d?+";
+    private static final String ID_REGEX = "^[0-9]\\d*";
+    private static final String NAME_REGEX = "^[A-Za-zА-ЯА-я]\\w+";
+    private static final String PRICE_REGEX = "\\d+\\.?\\d?+";
 
     public static boolean isValid(Map<String, String> params) {
-        return params.get(PRODUCT_NAME).matches(nameRegex)
-               && params.get(PRODUCT_PRICE).matches(priceRegex)
-               && params.get(PRODUCT_PRODUCER_ID).matches(idRegex);
+        return params.get(PRODUCT_NAME).matches(NAME_REGEX)
+               && params.get(PRODUCT_PRICE).matches(PRICE_REGEX)
+               && params.get(PRODUCT_PRODUCER_ID).matches(ID_REGEX);
     }
 }
